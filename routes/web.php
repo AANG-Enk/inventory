088e2b1dda603cea79b674fc5ee59d3c7da57551
AANG-Enk <farhanmohaemin14@gmail.com>
2024-07-22 21:27:34 +0700

    Set Up Route dan Controller Auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['guest'])->group(function () {
+    Route::prefix('login')->group(function () {
+        Route::get('/',[App\Http\Controllers\Auth\LoginController::class, 'index'])->name('login.index');
+        Route::post('/',[App\Http\Controllers\Auth\LoginController::class, 'store'])->name('login.store');
+    });
+});
+
+Route::middleware(['auth','verified'])->group(function () {
+    Route::get('/dashboard',[App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+
+    Route::resource('users', App\Http\Controllers\UserController::class);
+    Route::resource('roles', App\Http\Controllers\RoleController::class);
+    Route::resource('permissions', App\Http\Controllers\PermissionController::class);
+
+    Route::resource('satuans', App\Http\Controllers\SatuanController::class);
+    Route::resource('barangs', App\Http\Controllers\BarangController::class);
+    Route::resource('barangsmasuk', App\Http\Controllers\BarangMasukController::class);
+    Route::resource('barangskeluar', App\Http\Controllers\BarangKeluarController::class);
+});
